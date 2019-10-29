a64ffcb0589836cbe4286016e3426e3b89cc5208
svyatoslavslobodyan <svyatoslav.slobodyan16@my.stjohns.edu>
2019-10-29 07:43:22 -0400

    Added User Login working with DB Add User Use-Case Complete

--- app/forms.py
+++ app/forms.py
@@ -1,19 +1,35 @@
 from flask_wtf import FlaskForm, Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
+
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    user = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
 
 class AddVehicle(Form):
     make = StringField('Make', validators=[DataRequired()])
     model = StringField('Model', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-# class RegistrationForm(Form):
-#     make = StringField('make', validators=[DataRequired()])
-#     model = StringField('model', validators=[DataRequired()])
-    #color = StringField('New Password', validators=[DataRequired()])
+
+class RegistrationForm(FlaskForm):
+    user = StringField('User', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_user(self, user):
+        user = User.query.filter_by(user=user.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different name')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email')
--- app/routes.py
+++ app/routes.py
@@ -1,41 +1,63 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm, AddVehicle
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import LoginForm, AddVehicle, RegistrationForm
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'user_name': 'Tom'}
     cars = [
         {
-            'owner': {'user_name': 'John'},
+            'owner': {'user': 'John'},
             'car': 'Testing Car String'
         },
         {
-            'owner': {'user_name': 'Doe'},
+            'owner': {'user': 'Doe'},
             'car': 'Testing Car String2'
         }
     ]
-    return render_template('index.html', title='Home', user=user, cars=cars)
+    return render_template('index.html', title='Home', cars=cars)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(user=form.user.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
         return redirect(url_for('index'))
     return render_template('login.html',  title='Sign In', form=form)
 
-# @app.route('/add_vehicle', methods=['GET', 'POST']) #### Not Working Yet
-# def add_vehicle():
-#     form = AddVehicle()
-#     if form.validate_on_submit():
-#         flash('Add requested for car {}, remember_me={}'.format(
-#             form.make.data, form.model.data))
-#         return redirect(url_for('index'))
-#     return render_template('addVehicle.html',  title='Add Vehicle', form=form)
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(user=form.user.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You are now signed up to use our App!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 @app.route('/addVehicle', methods=['GET', 'POST'])
 def RegisterCar():
