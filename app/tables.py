c70aba89bc01ac04489273a79b22db3d3254c8a2
unknown <sarah.guthrie16@stjohns.edu>
2019-11-15 14:15:03 -0500

    Deleted table.py

--- app/tables.py
+++
@@ -1,9 +0,0 @@
-from flask_table import Table, Col
-
-
-class Schedules(Table):
-    id = Col('Id', show=False)
-    user = Col('User')
-    mechanic = Col('Mechanic')
-    appointment_date = Col('Appointment Date')
-    appointment_time = Col('Appointment Time')
